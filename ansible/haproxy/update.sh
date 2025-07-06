0894ffc227859dbebe4a3d65d92f2bc27c4a4034
Justin Toniazzo <jt@thoughtbot.com>
2025-07-06 13:22:42 -0400

    ignore ansible-lint errors about sops file
    I can't edit this file since it's autogeneated and encrypted by sops, so
    I can't fix these errors.

--- ansible/haproxy/update.sh
+++ ansible/haproxy/update.sh
@@ -4,6 +4,5 @@ tmpfile=$(mktemp)
 mv "$tmpfile" "$tmpfile.yaml"
 tmpfile="${tmpfile}.yaml"
 sops decrypt ./hosts.yaml > "$tmpfile"
-cat "$tmpfile"
 ansible-playbook -i "$tmpfile" ./playbook.yaml
-rm $tmpfile
+rm "$tmpfile"
