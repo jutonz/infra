d9fbae1ff4df3aef53513db0e1dab1a0c00bb9f7
Justin Toniazzo <jutonz@hey.com>
2026-01-07 13:00:14 -0500

    Setup keepalived for HA haproxy  (#92)
    * each k8s node runs haproxy
    * haproxy listens on 64430 and 4430
    * Traffic on 64430 is sent to any k3s master node on port 6443 (the k8s
    control port)
    * This allows me to put a floating IP (VIP) in my ~/.kube/config so I
    can still run kubectl commands when doing maintenance on a node.
    * Traffic on 4430 is also sent to k8s on port 443 and is used for
    traffic to pods

--- ansible/haproxy/letsencrypt/renewal-hooks/deploy/restart-haproxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-ls -l /etc/letsencrypt/live/ | grep ^d | awk '{print $9}' | while read -r domain; do
-  cat /etc/letsencrypt/live/$domain/fullchain.pem /etc/letsencrypt/live/$domain/privkey.pem > /etc/haproxy/certs/$domain.pem
-done
-
-systemctl reload haproxy
